5957c704faac08b3d8c9089d0e72fa0e15737b21
AbelPauw <114990624+AbelPauw@users.noreply.github.com>
2023-06-16 14:06:34 +0200

    complete revamp

--- wordpress-6.2-nl_NL/wordpress/wp-content/themes/abel/index.php
+++ wordpress-6.2-nl_NL/wordpress/wp-content/themes/abel/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
     <meta charset="UTF-8">
@@ -11,34 +12,40 @@
     <!-- De onderstaande regel code gebruikt de WordPress-functie get_stylesheet_uri() om de URL van het huidige thema's style.css-bestand op te halen en dit te koppelen aan het stijlblad van je website. -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-    
+
 </head>
 
 <body>
     <header>
-       
+
         <nav>
-            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <!-- De onderstaande regel code gebruikt de WordPress-functie  -->
-            <?php
-                 wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
-            ?>
+            <div class="title">
+                <figure class="wp-block-image size-full is-resized"><img src="http://localhost/wordpress-theme/wordpress-6.2-nl_NL/wordpress/wp-content/uploads/2023/06/transp-thyla.png" alt="" class="wp-image-18" width="126" height="106" /></figure>
+                <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <?php
+                wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
+                ?>
+            </div>
         </nav>
+        <div class="attention">
+            <h2>Lees onze meest recente posts</h2>
+            <a href="http://localhost/wordpress-theme/wordpress-6.2-nl_NL/wordpress/2023/06/16/de-voordelen-van-regelmatige-lichaamsbeweging/">lees meer</a>
+        </div>
     </header>
 
     <main>
-    <!-- De onderstaande regels code kijkt of en hoeveel posts er zijn en plaatst deze op de pagina.  -->
-    <?php
+        <!-- De onderstaande regels code kijkt of en hoeveel posts er zijn en plaatst deze op de pagina.  -->
+        <?php
         if (have_posts()) :
             while (have_posts()) :
                 the_post();
-                ?>
+        ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <div class="entry-content">
                         <?php the_content(); ?>
                     </div>
                 </article>
-                <?php
+            <?php
             endwhile;
         else :
             ?>
@@ -47,17 +54,18 @@
     </main>
 
     <footer>
-            <!-- Voeg hier je footerinhoud toe -->
-            <div class="navigation">
+        <!-- Voeg hier je footerinhoud toe -->
+        <div class="navigation">
             <h5>navigation:</h5>
             <?php
-                 wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
+            wp_nav_menu(array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'site-navigation'));
             ?>
-            </div>
-            <div class="coppyright">
+        </div>
+        <div class="coppyright">
             <h5>coppyright:</h5>
             <p>© 2023 - Abel Pauw</p>
-            </div>
+        </div>
     </footer>
 </body>
+
 </html>
---
+++ wordpress-project2/wp-content/themes/abelpw-theme/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <div class="container">
+            <h1><?php bloginfo('name'); ?></h1>
+            <p><?php bloginfo('description'); ?></p>
+        </div>
+    </header>
+
+    <nav>
+        <div class="container">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'menu_class' => 'primary-menu',
+            ));
+            ?>
+        </div>
+    </nav>
